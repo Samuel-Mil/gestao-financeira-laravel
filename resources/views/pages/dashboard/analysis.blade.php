8b4d8f687e2217c0862f6eba9b862eff34192022
Samuel Milhomens <samuel.mil.mar@gmail.com>
2023-10-23 16:21:34 -0300

    Remove lists in analysis page

--- resources/views/pages/dashboard/analysis.blade.php
+++ resources/views/pages/dashboard/analysis.blade.php
@@ -20,25 +20,13 @@
         <p>Atualmente o painel tem <b>{{$usersCount}}</b> usuários ativos.</p>
         <h3>Contas bancarias</h3>
         <p>Atualmente o painel tem <b>{{$bankCount}}</b> contas bancarias registradas, sendo elas:</p>
-        <ul>
-            @foreach ($banks as $bank)
-                <li>{{$bank['number']}}</li>
-            @endforeach
-        </ul>
+        
         <h3>Transações</h3>
         <p>Ao total foram <b>{{$transactionsCount}}</b> transções e foram movimentados R$<b>{{$amount}}</b> das seguintes contas:</p>
-        <ul>
-            @foreach ($transactions as $transaction)
-                <li>{{$transaction['outgoingAccount']['number']}} para {{$transaction['entryAccount']['number']}}</li>
-            @endforeach
-        </ul>
+        
         <h3>Faturas</h3>
         <p>Atualmente temos o total de <b>{{$invoicesCount}}</b> faturas emitidas sendo elas e seus status:</p>
-        <ul>
-            @foreach ($invoices as $invoice)
-                <li>{{$invoice['numero_fatura']}} - {{$invoice['status']}}</li>
-            @endforeach
-        </ul>
+        
     </div>
 
 </div>
